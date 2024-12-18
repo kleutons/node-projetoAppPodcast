4674c318f9f7f7384eaefc7a1a88e25e9cc36c59
kleuton novais <106082564+kleutons@users.noreply.github.com>
2024-12-18 11:31:49 -0300

    create filter epsodes

--- src/repositories/podcast.repository.ts
+++ src/repositories/podcast.repository.ts
@@ -1,14 +1,17 @@
 import fs from "fs"
 import path from "path";
-import { Podcast } from "../models/podcast.model";
+import { PodcastModel } from "../models/podcast.model";
 
 
 const pathData = path.join(__dirname, "../repositories/podcasts.json");
 
-export const repoPodcast = async (): Promise<Podcast[]> => {
+export const repoPodcast = async (podcastName?:string): Promise<PodcastModel[]> => {
     const rawData = fs.readFileSync(pathData,"utf-8");
+    let jsonFile = JSON.parse(rawData);
 
-    const jsonFile = JSON.parse(rawData);
+    if(podcastName){
+        jsonFile = jsonFile.filter((item:PodcastModel)=> item.podcastName === podcastName)
+    }
 
     return jsonFile;
 }
