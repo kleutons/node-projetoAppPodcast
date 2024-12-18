e01076d2057e63292edd29e11380e626e93ae731
kleuton novais <106082564+kleutons@users.noreply.github.com>
2024-12-18 10:09:24 -0300

    Created Http Server

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,13 @@
-console.log("Hello World!");
+import * as http from "http";
+
+const server = http.createServer((request: http.IncomingMessage, response: http.ServerResponse) =>
+     {
+        
+     }
+);
+
+const port = process.env.PORT;
+
+server.listen(port, ()=>{
+    console.log("servidor iniciado na porta: "+port);
+})
