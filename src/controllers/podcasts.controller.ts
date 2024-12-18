30653cf4fe63f917f421656ced354fb74b1f9b98
kleuton novais <106082564+kleutons@users.noreply.github.com>
2024-12-18 11:17:35 -0300

    Create Repository

--- src/controllers/podcasts.controller.ts
+++ src/controllers/podcasts.controller.ts
@@ -1,6 +1,7 @@
 import { IncomingMessage, ServerResponse } from 'http';
 import { serviceListEpisodes } from '../services/list-episodes.service';
 
+
 export const getListEpisodes = async (req: IncomingMessage, res: ServerResponse) =>{
     
     const content = await serviceListEpisodes();
--- src/services/list-episodes.service.ts
+++ src/services/list-episodes.service.ts
@@ -1,20 +1,8 @@
+import { repoPodcast } from "../repositories/podcast.repository";
 
 export const serviceListEpisodes = async () => {
 
-    const data =  [
-        {
-            podcastName: "flow",
-            episode: "SEGREDOS E CURIOSIDADES DA SÉRIE DO SENNA",
-            videoId: "13EWvfLeZJw",
-            categories: ["entretenimento", "esporte", "corrida"]
-        },
-        {
-            podcastName: "flow",
-            episode: "OS BASTIDORES DA DUBLAGEM DE DESENHOS ANIMADOS com WENDELL BEZERRA",
-            videoId: "eyeqGn4PnNE",
-            categories: ["entretenimento", "dublagem"]  
-        }
-    ];
+    const data = await repoPodcast();
 
     return data;
 };
