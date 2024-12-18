f4d1907a273ee51b1f16dd5e1b047ab9582dddae
kleuton novais <106082564+kleutons@users.noreply.github.com>
2024-12-18 15:41:54 -0300

    refactor server

---
+++ src/app.ts
@@ -0,0 +1,21 @@
+import * as http from "http";
+
+import {getFilterEpisodes, getListEpisodes} from './controllers/podcasts.controller'
+import { Routes } from "./routes/routes";
+import { HttpMethod } from "./utils/http-methods";
+
+export const app =  async (request: http.IncomingMessage, response: http.ServerResponse) =>{
+    
+    //queryString site.com/?x=string
+    // localhost:333/api/episode?p=flow
+    const [baseUrl, queryString] = request.url?.split("?") ?? ['', ''];
+
+
+    if(request.method === HttpMethod.GET && baseUrl === Routes.LIST){
+       await getListEpisodes(request, response);
+    }
+    
+    if(request.method === HttpMethod.GET && baseUrl === Routes.EPISODE){
+        await getFilterEpisodes(request, response);
+    }
+};
--- src/server.ts
+++ src/server.ts
@@ -1,28 +1,10 @@
 import * as http from "http";
+import { app } from "./app";
 
-import {getFilterEpisodes, getListEpisodes} from './controllers/podcasts.controller'
-import { Routes } from "./routes/routes";
-import { HttpMethod } from "./utils/http-methods";
-
-const server = http.createServer(
-   async (request: http.IncomingMessage, response: http.ServerResponse) =>{
-    
-    //queryString site.com/?x=string
-    // localhost:333/api/episode?p=flow
-    const [baseUrl, queryString] = request.url?.split("?") ?? ['', ''];
-
-
-    if(request.method === HttpMethod.GET && baseUrl === Routes.LIST){
-       await getListEpisodes(request, response);
-    }
-    
-    if(request.method === HttpMethod.GET && baseUrl === Routes.EPISODE){
-        await getFilterEpisodes(request, response);
-    }
-});
+const server = http.createServer(app);
 
 const port = process.env.PORT;
 
 server.listen(port, ()=>{
-    console.log("servidor iniciado na porta: "+port);
+    console.log("Servidor Iniciado: http://localhost:"+port);
 })
